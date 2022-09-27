f2aaf0e87034e78cd95ca82b769849f8690a439a
Teja Tadepalli <91416443+TejaTadepalli@users.noreply.github.com>
2022-09-27 23:45:11 +0530

    Update solution.cpp
    Added missing ';'

--- 1929. Concatenation of Array/solution.cpp
+++ 1929. Concatenation of Array/solution.cpp
@@ -6,7 +6,7 @@ class Solution
       const int n=nums.size();
       for(int i=0;i<n;i++)
       {
-        nums.push_back(nums[i])
+        nums.push_back(nums[i]);
       }
       return nums;
     }
